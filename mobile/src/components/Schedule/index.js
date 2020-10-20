0afbb0df543d4f37ae46fac75ccc5e8e871f2bee
Salv-Dev <salv.dev.br@gmail.com>
2020-10-20 18:53:04 -0300

    edit service form created

--- mobile/src/components/Schedule/index.js
+++ mobile/src/components/Schedule/index.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import CompleteService from './../../modals/CompleteService';
+import EditService from './../../modals/EditSchedule';
 
 import { Schedule, Date, Service, ClientName } from './styles';
 
 export default function ScheduleComponent({ content, completed }) {
-    const [modal, setModal] = useState(false);
+    const [CompleteServiceModal, setCompleteServiceModal] = useState(false);
+    const [EditServiceModal, setEditServiceModal] = useState(false);
 
     return (
         <>
-            <Schedule onPress={() => setModal(true)}>
+            <Schedule onPress={() => setCompleteServiceModal(true)} onLongPress={() => setEditServiceModal
+            (true)}>
                 <Date completed={completed}>{content.date}</Date>
                 <Service completed={completed}>{content.service}</Service>
                 <ClientName completed={completed}>{content.client_name}</ClientName>
@@ -16,8 +19,16 @@ export default function ScheduleComponent({ content, completed }) {
 
             { !completed && 
                 <CompleteService 
-                    show={modal}
-                    close={() => setModal(false)}
+                    show={CompleteServiceModal}
+                    close={() => setCompleteServiceModal(false)}
+                    content={content}
+                />
+            }
+
+            { !completed && 
+                <EditService
+                    show={EditServiceModal}
+                    close={() => setEditServiceModal(false)}
                     content={content}
                 />
             }
