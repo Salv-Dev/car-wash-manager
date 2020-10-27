cc27acd10f62054d0caf326e0e476d9d40aba8b6
Salv-Dev <salv.dev.br@gmail.com>
2020-10-27 18:46:18 -0300

    access denied modal created

--- mobile/src/components/Customer/index.js
+++ mobile/src/components/Customer/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AccessDeniedModal from './../../modals/AccessDenied';
 
 import { Customer, ClientName, ClientPhone } from './styles';
 
 export default function CustomerComponent({ content }) {
+    const [RemoveCustomerModal, setRemoveCustomerModal] = useState(false);
+    const [EditCustomerModal, setEditCustomerModal] = useState(false);
+
     return (
-        <Customer>
-            <ClientName>{content.name}</ClientName>
-            <ClientPhone>{content.phone}</ClientPhone>
-        </Customer>
+        <>
+            <Customer onPress={() => setRemoveCustomerModal(true)} onLongPress={() => setEditCustomerModal(true)}>
+                <ClientName>{content.name}</ClientName>
+                <ClientPhone>{content.phone}</ClientPhone>
+            </Customer>
+
+            <AccessDeniedModal 
+                show={RemoveCustomerModal}
+                close={() => setRemoveCustomerModal(false)}
+            />
+
+            <AccessDeniedModal 
+                show={EditCustomerModal}
+                close={() => setEditCustomerModal(false)}
+            />
+        </>
     )
 }
